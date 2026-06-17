16198eebf2e08121b7238ececb1f89da127badd9
waqar01-rd <waqarrodnani2@gmail.com>
2026-06-17 08:14:23 +0000

    Conversion of Datatypes

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,43 @@
+// let score = 44
+
+// console.log(typeof score); // number
+// console.log(typeof (score)); // number
+
+// let score = "44"
+
+// console.log(typeof score); // string 
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+
+// let score = "44abc"
+
+// console.log(typeof score); // string 
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+
+// "44" => 44
+// "44abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true, 0 => false
+// "" => false
+//"waqar" => ture
+
+let someNumber = 33
+
+let strNumber = String(someNumber)
+console.log(strNumber);
+console.log(typeof strNumber);
+
+
+
